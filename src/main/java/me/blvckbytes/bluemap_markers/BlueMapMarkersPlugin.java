d9de5f2ab42573295035b93c50a77177242bc307
Philip Hell <blvckbytes@gmail.com>
2025-01-30 16:13:50 +0100

    now bootstrapping as soon as the BlueMap-API becomes available; added timeout

--- src/main/java/me/blvckbytes/bluemap_markers/BlueMapMarkersPlugin.java
+++ src/main/java/me/blvckbytes/bluemap_markers/BlueMapMarkersPlugin.java
@@ -1,6 +1,7 @@
 package me.blvckbytes.bluemap_markers;
 
 import com.cryptomorin.xseries.XMaterial;
+import de.bluecolored.bluemap.api.BlueMapAPI;
 import me.blvckbytes.bluemap_markers.command.BlueMapMarkersCommand;
 import me.blvckbytes.bluemap_markers.config.BlueMapMarkersCommandSection;
 import me.blvckbytes.bluemap_markers.config.MainSection;
@@ -9,11 +10,13 @@ import me.blvckbytes.bluemap_markers.stores.ImageStore;
 import me.blvckbytes.bukkitevaluable.CommandUpdater;
 import me.blvckbytes.bukkitevaluable.ConfigKeeper;
 import me.blvckbytes.bukkitevaluable.ConfigManager;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Objects;
 import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class BlueMapMarkersPlugin extends JavaPlugin {
 
@@ -69,9 +72,33 @@ public class BlueMapMarkersPlugin extends JavaPlugin {
 
    */
 
+  private static final int API_ENABLE_TIMEOUT_S = 30;
+
   @Override
   public void onEnable() {
     var logger = getLogger();
+
+    var timeoutTask = Bukkit.getScheduler().runTaskLater(this, () -> {
+      logger.severe("Timed out while waiting for over " + API_ENABLE_TIMEOUT_S + "s the BlueMap-API to become available; disabling now!");
+      Bukkit.getPluginManager().disablePlugin(this);
+    }, 20L * API_ENABLE_TIMEOUT_S);
+
+    logger.info("Initialized a " + API_ENABLE_TIMEOUT_S + "s timeout to wait for the BlueMap-API to become available!");
+
+    // See: https://github.com/BlueMap-Minecraft/BlueMapAPI/wiki
+    BlueMapAPI.onEnable(api -> {
+      if (!isEnabled())
+        return;
+
+      timeoutTask.cancel();
+
+      logger.info("The BlueMap-API just became available; bootstrapping now!");
+
+      bootstrap(logger, api);
+    });
+  }
+
+  private void bootstrap(Logger logger, BlueMapAPI blueMap) {
 
     try {
       // First invocation is quite heavy - warm up cache
@@ -100,11 +127,9 @@ public class BlueMapMarkersPlugin extends JavaPlugin {
       var commandSendListener = new CommandSendListener(this, config);
 
       getServer().getPluginManager().registerEvents(commandSendListener, this);
-
-      // See: https://github.com/BlueMap-Minecraft/BlueMapAPI/wiki
-//      var mapApi = BlueMapAPI.getInstance().orElseThrow(() -> new IllegalStateException("Could not access the BlueMap-API"));
     } catch (Exception e) {
       logger.log(Level.SEVERE, "An error occurred while trying to enable the plugin!", e);
+      Bukkit.getPluginManager().disablePlugin(this);
     }
   }
 }
